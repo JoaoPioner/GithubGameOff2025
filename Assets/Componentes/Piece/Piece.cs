a1bd1da125187a988d6120dc5da7ec8f04238117
JoaoPioner <51408829+JoaoPioner@users.noreply.github.com>
2025-11-15 18:17:03 -0300

    Melhorias e conserto da IA + morte das peças

--- Assets/Componentes/Piece/Piece.cs
+++ Assets/Componentes/Piece/Piece.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class Piece : MonoBehaviour
 {
     [SerializeField] public float HP = 100f;
-    
-    
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     public void Damage(float hits)
@@ -19,6 +19,11 @@ public class Piece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (HP <= 0)
+        {
+            Debug.Log(gameObject.name + " foi destruído.");
+            Destroy(gameObject);
+        }
+
     }
 }
